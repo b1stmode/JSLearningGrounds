1e520f8fd32bac96d8b6641b861eb9657c624866
B1stmode <tima.gamedev@gmail.com>
2026-07-30 21:01:20 +0200

    basics - Basic Countdown

---
+++ 01-basics/006-CountDown/app.js
@@ -0,0 +1,30 @@
+/*
+Project: Count Down
+
+Concepts practiced:
+- Variables
+- Arithmetic operators
+- Console output
+- for/while loops
+
+Date:
+2026-07-30
+
+time spent on the drill: 4 minutes
+*/
+
+// for loop version:
+console.log("===For loop===")
+for (let i = 10; i >= 0; i--){
+    console.log(`${i}...`)
+} 
+console.log("GO!")
+
+// while loop version:
+console.log("===While loop===")
+let i = 10
+while (i >= 0){
+    console.log(`${i}...`)
+    i--
+}
+console.log("GO!")
