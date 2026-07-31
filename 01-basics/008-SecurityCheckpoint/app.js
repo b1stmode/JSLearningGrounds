51359e9982cd5d969fd10528dbb9bbdfa9842233
B1stmode <tima.gamedev@gmail.com>
2026-07-31 18:57:28 +0200

    basics - Security Checkpoint

---
+++ 01-basics/008-SecurityCheckpoint/app.js
@@ -0,0 +1,25 @@
+/*
+Project: Security Checkpoint
+
+Concepts practiced:
+- Variables
+- Arithmetic operators
+- Console output
+- for loops
+- break/continue statements
+
+
+Date:
+2026-07-31
+
+time spent on the drill:  minutes
+*/
+// variables 
+
+
+for (let i = 0; i <= 20; i++){
+    if(i % 4 ===0){
+        continue
+    }
+    console.log(`Checking ID ${i}`)
+}
