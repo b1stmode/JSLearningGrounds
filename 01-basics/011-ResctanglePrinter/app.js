67f50171283d3817ff2f74662f99171417ef3483
B1stmode <tima.gamedev@gmail.com>
2026-08-01 17:42:38 +0200

    basics - Rectangle Printer

---
+++ 01-basics/011-ResctanglePrinter/app.js
@@ -0,0 +1,23 @@
+/*
+Project: Rectangle Printer
+
+Concepts practiced:
+- Variables
+- Arithmetic operators
+- Console output
+- nested for loops
+
+
+Date:
+2026-08-01
+
+time spent on the drill: 3.5 minutes
+*/
+
+let Rows = 3
+let columns = 6
+
+let symbol = "*"
+
+    console.log(symbol.repeat(columns))
+}
